0d3e10f301714dd10950c20c603fa82a68209b3a
Kirill Varlamov <kirill@ongrid.pro>
2019-02-01 00:23:39 +0300

    Implement primitive tap interfaces

---
+++ contracts/Tap.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+/**
+ * @title Tap
+ * @dev Simple tap allowing spender to withdraw stablecoins at given rate
+ */
+contract Tap {
+    address public spender;
+    address public bucket;
+    string public description;
+    uint256 public rate;
+    uint256 public lastWithdraw;
+    uint256 public excessAmount;
+    bool public active;
+    address public registry;
+
+    /**
+     * @dev Constructor that configures the initial bucket state on deployment
+     */
+    constructor (address _spender, address _bucket, string memory _description, uint256 _rate, address _registry) public {
+        spender = _spender;
+        bucket = _bucket;
+        description = _description;
+        rate = _rate;
+        lastWithdraw = now;
+        registry = _registry;
+    }
+}
---
+++ test/Tap.test.js
@@ -0,0 +1,41 @@
+const { time } = require('openzeppelin-test-helpers');
+
+const Tap = artifacts.require('Tap');
+
+contract('Tap', function ([_, spender, bucket, registry]) {
+
+  before(async function () {
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by ganache
+    await time.advanceBlock();
+  });
+
+  beforeEach(async function () {
+    this.tap = await Tap.new(spender, bucket, 'Tap Name', 12345678, registry);
+    this.deployTime = (await time.latest());
+  });
+
+  it('has a spender', async function () {
+    (await this.tap.spender()).should.equal(spender);
+  });
+
+  it('has a bucket', async function () {
+    (await this.tap.bucket()).should.equal(bucket);
+  });
+
+  it('has a description', async function () {
+    (await this.tap.description()).should.equal('Tap Name');
+  });
+
+  it('has a rate', async function () {
+    (await this.tap.rate()).should.be.bignumber.equal('12345678');
+  });
+
+  it('has a registry', async function () {
+    (await this.tap.registry()).should.equal(registry);
+  });
+
+  it('has a current date', async function () {
+    (await this.tap.lastWithdraw()).should.be.bignumber.equal(this.deployTime);
+  });
+
+});
