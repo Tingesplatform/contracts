9d24822ca71c82b9ad57d20f329eb47c38c4fb8e
Kirill Varlamov <kirill@ongrid.pro>
2019-02-04 15:20:52 +0300

    NT-25 Claimable Registry

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
+
 /**
  * @title ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/20
@@ -8,19 +9,55 @@ interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
 }
 
+
 /**
  * @title Fund
  * @dev Very simple Fund example
  */
 contract Registry {
     string public name;
+    address public owner;
+    address public pendingOwner;
     mapping(address => bool) public stableCoins;
 
     /**
      * @dev Constructor
      */
-    constructor (string memory _name) public {
+    constructor (address _pendingOwner, string memory _name) public {
         name = _name;
+        pendingOwner = _pendingOwner;
+    }
+
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can do this.");
+        _;
+    }
+
+    /**
+    * @dev Modifier throws if called by any account other than the pendingOwner.
+    */
+    modifier onlyPendingOwner() {
+        require(msg.sender == pendingOwner, "Only pendingOwner can do this.");
+        _;
+    }
+
+    /**
+    * @dev Allows the current owner to set the pendingOwner address.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) public onlyOwner {
+        pendingOwner = newOwner;
+    }
+
+    /**
+    * @dev Allows the pendingOwner address to finalize the transfer.
+    */
+    function claimOwnership() public onlyPendingOwner {
+        owner = pendingOwner;
+        pendingOwner = address(0);
     }
 
     function withdrawStableCoin(IERC20 _stableCoin, address _spender, uint256 _value) public returns (bool) {
--- test/FullBehavior.test.js
+++ test/FullBehavior.test.js
@@ -3,10 +3,11 @@ const { should, ether } = require('openzeppelin-test-helpers');
 const Tap = artifacts.require('Tap');
 const Registry = artifacts.require('Registry');
 const Fund = artifacts.require('Fund');
+const ZERO = '0x0000000000000000000000000000000000000000';
 
-contract('FullBehaviorTest', function ([_, deployer, owner, wallet, investor]) {
+contract('FullBehaviorTest', function ([_, deployer, owner, newOwner]) {
   beforeEach(async function () {
-    this.reg = await Registry.new('Registry');
+    this.reg = await Registry.new(owner, 'Registry');
     this.fund = await Fund.new(this.reg.address, ether('10'), 'Fund');
     this.tap = await Tap.new(this.reg.address, this.fund.address, deployer, 123, 'Tap');
   });
@@ -14,5 +15,36 @@ contract('FullBehaviorTest', function ([_, deployer, owner, wallet, investor]) {
     should.exist(this.reg);
     should.exist(this.fund);
     should.exist(this.tap);
+  });
+  it('returns correct owner and pendingOwner', async function () {
+    (await this.reg.owner()).should.be.equal(ZERO);
+    (await this.reg.pendingOwner()).should.be.equal(owner);
+  });
+  describe('After claimed ownership', function () {
+    beforeEach(async function () {
+      await this.reg.claimOwnership({ from: owner });
+    });
+    it('returns correct owner', async function () {
+      (await this.reg.owner()).should.be.equal(owner);
+      (await this.reg.pendingOwner()).should.be.equal(ZERO);
+    });
+    describe('After transferred ownership to newOwner', function () {
+      beforeEach(async function () {
+        await this.reg.transferOwnership(newOwner, { from: owner });
+      });
+      it('returns correct owner', async function () {
+        (await this.reg.owner()).should.be.equal(owner);
+        (await this.reg.pendingOwner()).should.be.equal(newOwner);
+      });
+      describe('After claimed ownership by newOwner', function () {
+        beforeEach(async function () {
+          await this.reg.claimOwnership({ from: newOwner });
+        });
+        it('returns newOwner as owner', async function () {
+          (await this.reg.owner()).should.be.equal(newOwner);
+          (await this.reg.pendingOwner()).should.be.equal(ZERO);
+        });
+      });
+    });
   });
 });
